2c11e46c599ae151018a15fac84ccb6f29f8bacb
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-01-22 18:34:58 +0530

    Update dependecies.R

--- dependecies.R
+++ dependecies.R
@@ -3,6 +3,12 @@ suppressPackageStartupMessages({
   if (!require('magrittr')){install.packages('magrittr')}; library(magrittr)
   if (!require('ggplot2')){install.packages('ggplot2')}; library(ggplot2)
   if (!require('dplyr')){install.packages('dplyr')}; library(dplyr)
+  if (!require('RCurl')){install.packages('RCurl')}; library(RCurl)
+  if (!require('jsonlite')){install.packages('jsonlite')}; library(jsonlite)
+
+  if (!(require("h2o"))){ install.packages("h2o", type="source", 
+                 repos=(c("http://h2o-release.s3.amazonaws.com/h2o/latest_stable_R")))}
+  
   if (!require('h2o')){install.packages('h2o')}; library(h2o)
   if (!require('DT')){install.packages('DT')};library(DT)
   if (!require('markdown')){install.packages('markdown')};library(markdown)
